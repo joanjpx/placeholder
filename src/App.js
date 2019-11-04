d7a1a692e72734589a8185e7b0c7415ae8864c49
Joan P. Alvarado <joansyn.s@gmail.com>
2019-11-04 17:55:38 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Placeholder from "./PlaceholderComponent/Placeholder";
+import Card from "./CardComponent/Card";
+import FadeIn from "react-fade-in";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        setTimeout(() => this.setState({ loading: false }), 5000);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div class="d-flex flex-column justify-content-center align-items-center">
+            <FadeIn>
+              <h1 class="mb-5" style={{ color: "white" }}>
+                Placeholder Example
+                <img
+                  src={logo}
+                  width={"100px"}
+                  className={"App-logo"}
+                  alt="logo"
+                />
+              </h1>
+            </FadeIn>
+            <div class="card shadow-lg p-3">
+              <div style={{ width: "317.75px" }}>
+                {this.state.loading ? (
+                  <>
+                    <Placeholder />
+                    <Placeholder />
+                    <Placeholder />
+                  </>
+                ) : (
+                  <FadeIn>
+                    <Card
+                      song={"God's Plan"}
+                      artist={"Drake"}
+                      image={
+                        "https://i.pinimg.com/originals/e3/f2/0f/e3f20f0243a2102b5d8de3c7d44bcc14.jpg"
+                      }
+                    />
+                    <Card
+                      song={"One Dance"}
+                      artist={"Drake"}
+                      image={
+                        "https://upload.wikimedia.org/wikipedia/en/a/af/Drake_-_Views_cover.jpg"
+                      }
+                    />
+                    <Card
+                      song={"Hotline Bling"}
+                      artist={"Drake"}
+                      image={
+                        "https://www.billboard.com/files/styles/article_main_image/public/media/drake-hotline-bling-video-still-2015-billbaord-650.jpg"
+                      }
+                    />
+                  </FadeIn>
+                )}
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
